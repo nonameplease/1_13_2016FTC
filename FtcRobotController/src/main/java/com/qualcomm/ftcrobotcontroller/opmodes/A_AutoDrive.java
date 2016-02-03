cb7ed619e78f317f0177859d8dc3dc05150fba44
Scott Shao <dyshao1997@gmail.com>
2016-02-02 18:56:10 -0600

    2/2/16

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/A_AutoDrive.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/A_AutoDrive.java
@@ -1,6 +1,8 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
@@ -9,5 +11,31 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class A_AutoDrive extends A_RobotDrive {
     public A_AutoDrive(DcMotor left, DcMotor right, DcMotor leftrotate, DcMotor rightrotate, Servo Climber, Servo ResQ) {
         super(left, right, leftrotate, rightrotate, Climber, ResQ);
+    }
+
+    DeviceInterfaceModule dim;
+    ColorSensor color;
+
+    //change the value if necessary LOL
+    final static int ENCODER_CPR = 1120;
+    final static int GEAR_RATIO = 1;
+    final static int WHEEL_DIAMETER = 4;
+    final static int DISTANCE = 36; //in inches
+
+    final static double CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
+    final static double ROTATIONS = DISTANCE / CIRCUMFERENCE;
+    final static double COUNTS = ENCODER_CPR * ROTATIONS * GEAR_RATIO;
+    final static int COUNTSINT = (int) COUNTS * -1;
+
+    public A_AutoDrive(DeviceInterfaceModule Dim, ColorSensor Color)
+    {
+        super(left, right, leftrotate, rightrotate, Climber, ResQ);
+        dim = Dim;
+        color = Color;
+    }
+
+    public void encoderDrive()
+    {
+
     }
 }
