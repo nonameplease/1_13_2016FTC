37b211cb0971467e0fb1518e2e4348eb86c04f7f
Scott Shao <dyshao1997@gmail.com>
2016-02-02 19:24:52 -0600

    2/2/16

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/A_AutoDrive.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/A_AutoDrive.java
@@ -1,7 +1,10 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import android.bluetooth.BluetoothClass;
+
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -9,14 +12,11 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Created by Scott on 1/31/2016.
  */
 public class A_AutoDrive extends A_RobotDrive {
-    public A_AutoDrive(DcMotor left, DcMotor right, DcMotor leftrotate, DcMotor rightrotate, Servo Climber, Servo ResQ) {
-        super(left, right, leftrotate, rightrotate, Climber, ResQ);
-    }
 
     DeviceInterfaceModule dim;
     ColorSensor color;
 
-    //change the value if necessary LOL
+    /*change the value if necessary LOL
     final static int ENCODER_CPR = 1120;
     final static int GEAR_RATIO = 1;
     final static int WHEEL_DIAMETER = 4;
@@ -26,16 +26,33 @@ public class A_AutoDrive extends A_RobotDrive {
     final static double ROTATIONS = DISTANCE / CIRCUMFERENCE;
     final static double COUNTS = ENCODER_CPR * ROTATIONS * GEAR_RATIO;
     final static int COUNTSINT = (int) COUNTS * -1;
+    */
 
-    public A_AutoDrive(DeviceInterfaceModule Dim, ColorSensor Color)
-    {
+    public A_AutoDrive(DcMotor left, DcMotor right, DcMotor leftrotate, DcMotor rightrotate, Servo Climber, Servo ResQ, DeviceInterfaceModule Dim, ColorSensor Color) {
         super(left, right, leftrotate, rightrotate, Climber, ResQ);
         dim = Dim;
         color = Color;
+        leftMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        rightMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
     }
 
-    public void encoderDrive()
+    public int getTargetCounts(int inches)
     {
+        final int ENCODER_CPR = 1120;
+        final int GEAR_RATIO = 1;
+        final int WHEEL_DIAMETER = 4;
+        final int DISTANCE = inches; //in inches
 
+        final double CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
+        final double ROTATIONS = DISTANCE / CIRCUMFERENCE;
+        final double COUNTS = ENCODER_CPR * ROTATIONS * GEAR_RATIO;
+        final int COUNTSINT = (int) COUNTS;
+        return COUNTSINT;
+    }
+
+    public void encoderDrive(int COUNTSINT)
+    {
+        leftMotor.setTargetPosition(COUNTSINT);
+        rightMotor.setTargetPosition(COUNTSINT);
     }
 }
